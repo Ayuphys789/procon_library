6e8abdf85113e0e1353c48f9e77ea8d5ebc90ef7
Ayuphys789 <119868044+Ayuphys789@users.noreply.github.com>
2022-12-12 00:52:41 +0900

    Create isPrime.cpp

---
+++ isPrime.cpp
@@ -0,0 +1,19 @@
+//Nが素数かどうか√Nで判定
+
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+bool isPrime(ll n) {
+    if (n <= 1) return false;
+    for (ll p = 2; p * p <= n; ++p) {
+        if (n % p == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n; cin >> n;
+    if (isPrime(n)) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
