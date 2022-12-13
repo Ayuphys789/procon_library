0c7faf86234b2242a53467da70e2b789e269ccbd
Ayuphys <119868044+Ayuphys789@users.noreply.github.com>
2022-12-13 13:21:21 +0900

    Update modPow.cpp

--- NumberTheory/modPow.cpp
+++ NumberTheory/modPow.cpp
@@ -1,8 +1,21 @@
-//(N^P)%modをO(logN)で計算
 
-#include<bits/stdc++.h>
+// x ^ a を MOD で割った余りを O(logN) で求める
+
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define REP(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define FOR(i, a, b) for (ll i = a; i <= b; i++)
+#define bit(x, i) (((x) >> (i)) & 1)
+#define all(x) x.begin(), x.end()
+#define fcout cout << fixed << setprecision(15)
+#define PI 3.1415926535897932384626433832795028841971693993751058209749445923078160628620899
+const ll INF = 1000000000000000000;
+const ll NIL = -1;
+const ll MOD = 998244353;
+const ll dx[4] = {0, 1, 0, -1};
+const ll dy[4] = {1, 0, -1, 0};
 
 ll modPow(ll x, ll a) {
   if (a == 1) return x;
