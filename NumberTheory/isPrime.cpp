26950edf0ede3a9d15ba95f16f59ed81bdeceaa1
Ayuphys <119868044+Ayuphys789@users.noreply.github.com>
2022-12-13 13:35:57 +0900

    Update isPrime.cpp

--- NumberTheory/isPrime.cpp
+++ NumberTheory/isPrime.cpp
@@ -1,19 +1,30 @@
-//Nが素数かどうかO(√N)で判定
 
-#include <iostream>
+// N が素数かどうか O(√N) で判定
+
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define REP(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define FOR(i, a, b) for (ll i = a; i <= b; i++)
+#define bit(x, i) (((x) >> (i)) & 1)
+#define all(x) x.begin(), x.end()
+#define fcout cout << fixed << setprecision(15)
+#define PI 3.1415926535897932384626433832795028841971693993751058209749445923078160628620899
+const ll INF = 1000000000000000000;
+const ll NIL = -1;
+const ll MOD = 998244353;
+const ll dx[4] = {0, 1, 0, -1};
+const ll dy[4] = {1, 0, -1, 0};
 
-bool isPrime(ll n) {
-    if (n <= 1) return false;
-    for (ll p = 2; p * p <= n; ++p) {
-        if (n % p == 0) return false;
+bool isPrime(long long n)
+{
+    if (n <= 1)
+        return false;
+    for (long long p = 2; p * p <= n; ++p)
+    {
+        if (n % p == 0)
+            return false;
     }
     return true;
-}
-
-int main() {
-    int n; cin >> n;
-    if (isPrime(n)) cout << "YES" << endl;
-    else cout << "NO" << endl;
 }
