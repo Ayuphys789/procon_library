f532a83a88cde3fd5303d7d6fd5197199aa20b2e
Ayuphys <119868044+Ayuphys789@users.noreply.github.com>
2022-12-13 13:24:30 +0900

    Update modInv.cpp

--- NumberTheory/modInv.cpp
+++ NumberTheory/modInv.cpp
@@ -1,6 +1,21 @@
-//Nの逆元を計算
+
+// x の MOD での逆元を求める
+
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define REP(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define FOR(i, a, b) for (ll i = a; i <= b; i++)
+#define bit(x, i) (((x) >> (i)) & 1)
+#define all(x) x.begin(), x.end()
+#define fcout cout << fixed << setprecision(15)
+#define PI 3.1415926535897932384626433832795028841971693993751058209749445923078160628620899
+const ll INF = 1000000000000000000;
+const ll NIL = -1;
+const ll MOD = 998244353;
+const ll dx[4] = {0, 1, 0, -1};
+const ll dy[4] = {1, 0, -1, 0};
 
 ll modPow(ll x, ll a) {
   if (a == 1) return x;
@@ -9,6 +24,9 @@ ll modPow(ll x, ll a) {
   return (t * t) % MOD;
 }
 
+ll modInv(ll x) {
+  return modPow(x, MOD - 2);
+}
 ll modInv(ll x) {
   return modPow(x, MOD - 2);
 }
